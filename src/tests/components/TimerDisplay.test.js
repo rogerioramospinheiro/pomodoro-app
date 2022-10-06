eaf275a65da5d6078c272e7bde0f7d5b39dab5c7
Rogerio Pinheiro <rogerioramospinheiro@gmail.com>
2022-10-06 12:12:13 -0300

    Add unit test scenario for init timer

--- src/tests/components/TimerDisplay.test.js
+++ src/tests/components/TimerDisplay.test.js
@@ -33,3 +33,33 @@ test('Should update timer when user canceled timer', () => {
     expect(setCounter.mock.calls[0][1]).toBe(0);
     expect(setCounter.mock.calls[0][2]).toBe(0);
 });
+
+test('Should update timer end time when init timer', () => {
+    // Given
+    const setCounter = jest.fn();
+    const ctx = {
+        state: {
+            minutes: 0,
+            seconds: 0,
+            endTime: 0
+        },
+        props: {
+            init_time: 1
+        },
+        setCounter
+    };
+    const calculate = jest.fn(() => ({
+        minutes: 1,
+        seconds: 0,
+        end_time: 60 * 1000,
+        is_counting: true,
+        is_finished: false
+    }));
+    // When
+    updateTimer(ctx, calculate);
+    // Then
+    expect(setCounter.mock.calls.length).toBe(1);
+    expect(setCounter.mock.calls[0][0]).toBe(1);
+    expect(setCounter.mock.calls[0][1]).toBe(0);
+    expect(setCounter.mock.calls[0][2]).toBe(60 * 1000);
+});
