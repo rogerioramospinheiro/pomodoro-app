c2be5b7f04285eaca3ca930a54e3b6978e4352ea
Rogerio Pinheiro <rogerioramospinheiro@gmail.com>
2022-10-10 21:27:57 -0300

    Add should finish countdown unit test

--- src/tests/components/TimerDisplay.test.js
+++ src/tests/components/TimerDisplay.test.js
@@ -17,7 +17,7 @@ test('Should update timer when user canceled timer', () => {
         state: {
             minutes: 0,
             seconds: 30,
-            endTime: 0
+            endtime: 0
         },
         props: {
             init_time: 0
@@ -41,7 +41,7 @@ test('Should update timer end time when init timer', () => {
         state: {
             minutes: 0,
             seconds: 0,
-            endTime: 0
+            endtime: 0
         },
         props: {
             init_time: 1
@@ -62,4 +62,39 @@ test('Should update timer end time when init timer', () => {
     expect(setCounter.mock.calls[0][0]).toBe(1);
     expect(setCounter.mock.calls[0][1]).toBe(0);
     expect(setCounter.mock.calls[0][2]).toBe(60 * 1000);
+});
+
+test('Should finish countdown', () => {
+    // Given
+    const setCounter = jest.fn();
+    const finishCountdown = jest.fn();
+    const ctx = {
+        state: {
+            minutes: 0,
+            seconds: 0,
+            endtime: 60 * 1000
+        },
+        props: {
+            init_time: 1
+        },
+        setCounter,
+        finishCountdown
+    };
+    const calculate = jest.fn(() => ({
+        minutes: 0,
+        seconds: 0,
+        end_time: 60 * 1000,
+        is_counting: false,
+        is_finished: true
+    }));
+    // When
+    updateTimer(ctx, calculate);
+    // Then
+    expect(setCounter.mock.calls.length).toBe(1)
+    expect(setCounter.mock.calls[0][0]).toBe(0);
+    expect(setCounter.mock.calls[0][1]).toBe(0);
+    expect(setCounter.mock.calls[0][2]).toBe(0);
+
+    expect(finishCountdown.mock.calls.length).toBe(1);
+    expect(finishCountdown.mock.calls[0][0]).toBe(60 * 1000);
 });
